a460cb1a4a4328939f9afc2982908cda2d120fc5
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2020-03-25 16:22:38 +0100

    x

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 3000;
 
 const overviewRouter = require("./routes/overview");
 const detailRouter = require("./routes/detail");
@@ -20,7 +20,9 @@ app.use(detailRouter);
 // app.listen(function(port) {
 //   console.log("server is running" + port);
 // });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
 
 // app.get("/", (req, res) => {
 //   paintingData.getPaintings().then(function(paintingData) {
